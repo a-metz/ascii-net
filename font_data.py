4ef2324f8bd6f2d6901c0fc26605928a0a7dcaf6
Alexander Metz <alexander.wt.metz@gmail.com>
2016-05-03 12:48:13 +0200

    refactor: font data as list

--- font_data.py
+++ font_data.py
@@ -1,24 +1,42 @@
-from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
-class Glyph:
-	def __init__(self, char, font, dim):
-		self.char = char
-		self.font = font
-		self.dim = dim
-		self.img = Image.new('L', dim, color=255)
-		dh = ImageDraw.Draw(self.img)
-		dh.text((0, 0), char, font=font, fill=0)
-		self.imgarray = np.asarray(self.img, dtype='float64')
+from PIL import Image, ImageDraw, ImageFont
 
-def generate_glyph_set(
-		char_set='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;?@[\\]^_`{|}~ ',
+
+class GlyphList(list):
+	def __init__(self, charset, fontname, fontsize, dim):
+		list.__init__(self)
+
+		self.charset = charset
+		self.num_pixels = dim[0] * dim[1]
+
+		font = ImageFont.truetype(fontname, fontsize)
+
+		# generate glyphs
+		for index, char in enumerate(charset):
+			g = Glyph()
+			g.index = index
+			g.char = char
+			g.image = generate_image(char, font, dim)
+			g.array = np.asarray(g.image, dtype='float64').flatten() / 255
+
+			self.append(g)
+
+
+class Glyph:
+	pass
+
+
+def generate_image(char, font, dim):
+	img = Image.new('L', dim, color=255)
+	dh = ImageDraw.Draw(img)
+	dh.text((0, 0), char, font=font, fill=0)
+	return img
+
+
+def generate_default_glyphs():
+	return GlyphList(	
+		charset='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;?@[\\]^_`{|}~ ',
 		fontname='DejaVuSansMono.ttf',
 		fontsize=18,
-		dim=(11, 23)
-		):
-	glyph_set = []
-	font = ImageFont.truetype(fontname, fontsize)
-	for char in char_set:
-		glyph_set.append(Glyph(char, font, dim))
-	return glyph_set
+		dim=(11, 23))
