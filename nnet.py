96bce8e78176555a411858f8f3f9bd6d5e06913f
Alexander Metz <alexander.wt.metz@gmail.com>
2016-05-09 18:21:04 +0200

    add weight factor to compensate for number of inputs

--- nnet.py
+++ nnet.py
@@ -13,8 +13,9 @@ class Layer:
 	# i number of inputs
 	# self.w weight matrix
 	def __init__(self, num_neurons, num_inputs, weight_transform=lambda w: w):
+		self.weight_factor = 1 / np.sqrt(num_inputs)
 		# initialize random weight matrix
-		self.w = (2 * np.random.random((num_neurons, num_inputs))) - 1
+		self.w = (np.random.random((num_neurons, num_inputs)) - 0.5) * self.weight_factor
 		self.numpy_weight_transform = np.vectorize(weight_transform)
 
 	# self.x input vector
@@ -32,7 +33,7 @@ class Layer:
 
 	# self.dE_y error derivative vector
 	# l learning rate
-	def back_propagate(self, error, learning_rate=1):
+	def back_propagate(self, error, learning_rate):
 		dE_y = error
 		# error derivative vector with regard to sum of weighted inputs z
 		dE_z = np.multiply(nonlin_deriv(self.y), dE_y)
@@ -43,7 +44,7 @@ class Layer:
 		# error derivative with regard to weight matrix, for correction of weight matix
 		dE_w = np.multiply(self.x, np.matrix(dE_z).T)
 		# correct original weights by learning rate
-		self.w += np.multiply(learning_rate, dE_w)
+		self.w += np.multiply(learning_rate * self.weight_factor, dE_w)
 
 		return dE_x
 
@@ -54,4 +55,4 @@ def nonlin(z):
 
 # derivative of nonlinear function, dy/dz
 def nonlin_deriv(y):
-	return y * (1 - y)
+	return np.multiply(y, 1 - y)
