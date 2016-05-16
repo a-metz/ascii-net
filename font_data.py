005eda3a0cc7c5fc5648e00ceade0c1aec403e6b
Alexander Metz <alexander.wt.metz@gmail.com>
2016-05-16 12:27:10 +0200

    generate numpy array inputs and labels for keras

--- font_data.py
+++ font_data.py
@@ -3,40 +3,39 @@ import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 
 
-class GlyphList(list):
-	def __init__(self, charset, fontname, fontsize, dim):
-		list.__init__(self)
-
-		self.charset = charset
-		self.num_pixels = dim[0] * dim[1]
-
-		font = ImageFont.truetype(fontname, fontsize)
-
-		# generate glyphs
-		for index, char in enumerate(charset):
-			g = Glyph()
-			g.index = index
-			g.char = char
-			g.image = generate_image(char, font, dim)
-			g.array = np.asarray(g.image, dtype='float64').flatten() / 255
-
-			self.append(g)
-
-
-class Glyph:
-	pass
-
-
 def generate_image(char, font, dim):
-	img = Image.new('L', dim, color=255)
-	dh = ImageDraw.Draw(img)
-	dh.text((0, 0), char, font=font, fill=0)
-	return img
+    img = Image.new('L', dim, color=255)
+    dh = ImageDraw.Draw(img)
+    dh.text((0, 0), char, font=font, fill=0)
+    return img
 
 
-def generate_default_glyphs():
-	return GlyphList(	
-		charset='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;?@[\\]^_`{|}~ ',
-		fontname='fonts/DejaVuSansMono.ttf',
-		fontsize=18,
-		dim=(11, 23))
+def generate_data(charset, fontname, fontsize, dim):
+    num_datasets = len(charset)
+
+    num_pixels = dim[0] * dim[1]
+    num_classes = len(charset)
+
+    inputs = np.zeros((num_datasets, num_pixels))
+    labels = np.zeros((num_datasets, num_classes))
+
+    font = ImageFont.truetype(fontname, fontsize)
+
+    for index, char in enumerate(charset):
+        inputs[index, :] = np.asarray(
+            generate_image(char, font, dim),
+            dtype='float32').flatten() / 255
+        label = np.zeros(num_classes)
+        label[index] = 1
+        labels[index, :] = label
+
+    return inputs, labels
+
+
+def generate_default_data():
+    return generate(
+        charset=
+        '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;?@[\\]^_`{|}~ ',
+        fontname='fonts/DejaVuSansMono.ttf',
+        fontsize=18,
+        dim=(11, 23))
