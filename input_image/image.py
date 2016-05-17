4c4f9f05923fd2d624e07f5e2b925fcd82c3405e
Alexander Metz <alexander.wt.metz@gmail.com>
2016-05-17 16:50:37 +0200

    fix input image dimensions, add test

--- input_image/image.py
+++ input_image/image.py
@@ -7,10 +7,10 @@ def read(filename, sample_width, sample_height):
     y_off = 0
     im = Image.open(filename).convert('L')
 
-    for y_off in range(0, im.height - sample_height, sample_height):
-        for x_off in range(0, im.width - sample_width, sample_width):
+    for y_off in range(0, im.height - sample_height + 1, sample_height):
+        for x_off in range(0, im.width - sample_width + 1, sample_width):
 
             # extract sample image from whole image
-            sample_im = im.crop((x_off, y_off, x_off + sample_width, y_off +
-                                 sample_height))
-            yield sample_im
+            sample = im.crop((x_off, y_off, x_off + sample_width, y_off +
+                              sample_height))
+            yield sample
