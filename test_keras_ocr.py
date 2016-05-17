63623754f9a5e2d1d3c7440122f421b4e6b0b8ab
Alexander Metz <alexander.wt.metz@gmail.com>
2016-05-17 16:43:00 +0200

    rename modules

--- test_keras_ocr.py
+++ test_keras_ocr.py
@@ -1,12 +1,12 @@
-from font_data import glyphs
-from keras_model import keras_ocr, keras_data
+from input_font import glyphs
+from keras_model import ocr, font_data
 
 
 def test_keras_ocr():
     g = list(glyphs.default_glyphs())
-    inputs, labels, chars = keras_data.convert(g)
+    inputs, labels, chars = font_data.convert(g)
 
-    m = keras_ocr.Model(len(inputs[0]), len(labels[0]))
+    m = ocr.Model(len(inputs[0]), len(labels[0]))
 
     for i in range(100):
         # train for 5 epochs
@@ -16,7 +16,7 @@ def test_keras_ocr():
         p_classes = m.predict(inputs)
 
         # get wrongly predicted chars
-        p_chars = keras_data.deconvert(chars, p_classes)
+        p_chars = font_data.deconvert(chars, p_classes)
         p_errors = [c for c, pc in zip(chars, p_chars) if c != pc]
 
         print('predict:', ''.join(p_chars))
