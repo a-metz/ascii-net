f58990b922bf1774e090e8f29b07eec05f6bc49c
Alexander Metz <alexander.wt.metz@gmail.com>
2016-05-30 21:50:20 +0200

    nicer output while training

--- test_keras_ocr.py
+++ test_keras_ocr.py
@@ -3,44 +3,41 @@ from input_image import image
 from keras_model import ocr, font_data, image_data
 
 
-def test_train():
-    g = list(glyphs.default_glyphs())
-    inputs, labels, chars = font_data.convert(g)
-
-    m = ocr.Model(len(inputs[0]), len(labels[0]))
-
-    for i in range(100):
+def train(model, inputs, labels, chars, epochs, e_output=100):
+    for i in range(0, epochs, e_output):
         # train for 5 epochs
-        m.train(inputs, labels, 5)
+        model.train(inputs, labels, e_output)
 
         # predict class for all training data
-        p_classes = m.predict(inputs)
+        p_classes = model.predict(inputs)
 
         # get wrongly predicted chars
         p_chars = font_data.deconvert(chars, p_classes)
         p_errors = [c for c, pc in zip(chars, p_chars) if c != pc]
 
-        print('predict:', ''.join(p_chars))
-        print('errors: ', ''.join(p_errors))
+        print()
+        print('epoch: %i' % (i + e_output))
+        print('predict: %s' % ''.join(p_chars))
+        print('errors: %s' % ''.join(p_errors))
 
-        if len(p_errors) == 0:
-            break
+        #if len(p_errors) == 0:
+        #    break
 
 
-def test_generate():
+def test_keras_ocr():
     g = list(glyphs.default_glyphs())
     inputs, labels, chars = font_data.convert(g)
 
     imgs = list(image.read('input_image/test_image_w.png', 9, 18))
     test_inputs = image_data.convert(imgs)
 
-    m = ocr.Model(len(inputs[0]), len(labels[0]))
+    model = ocr.Model(len(inputs[0]), len(labels[0]))
 
     # train for 100 epochs
-    m.train(inputs, labels, 1000)
+    train(model, inputs, labels, chars, 1000)
 
     # predict class for test_inputs
-    p_classes = m.predict(test_inputs)
+    p_classes = model.predict(test_inputs)
 
     p_chars = font_data.deconvert(chars, p_classes)
     for i in range(0, len(p_chars), 80):
@@ -48,4 +45,4 @@ def test_generate():
 
 
 if __name__ == "__main__":
-    test_generate()
+    test_keras_ocr()
