e1e4828fdeceeabbaec9ede24329e9fe6d2bca49
Alexander Metz <alexander.wt.metz@gmail.com>
2016-07-04 21:46:03 +0200

    add font_data test

--- font/font_data.py
+++ font/font_data.py
@@ -1,6 +1,8 @@
 import numpy as np
 
 
+# convert glyphs to numpy arrays of image data, one-hot class labels and
+# corresponding characters
 def convert(glyphs):
     # infer dimensions from glyphs
     num_pixels = glyphs[0].width * glyphs[0].height
@@ -8,22 +10,23 @@ def convert(glyphs):
     num_datasets = num_classes
 
     # initialize arrays
-    inputs = np.zeros((num_datasets, num_pixels))
+    data = np.zeros((num_datasets, num_pixels))
     labels = np.zeros((num_datasets, num_classes))
     chars = []
 
     for index, glyph in enumerate(glyphs):
         # convert to flat numpy array
-        inputs[index, :] = np.asarray(glyph, dtype='float32').flatten() / 255
-        # generate multinomial class label
+        data[index, :] = np.asarray(glyph, dtype='float32').flatten() / 255
+        # generate one-hot class label
         label = np.zeros(num_classes)
         label[index] = 1
         labels[index, :] = label
 
         chars.append(glyph.char)
 
-    return inputs, labels, chars
+    return data, labels, chars
 
 
+# deconvert array of classes to characters
 def deconvert(chars, classes):
     return [chars[c] for c in classes]
