680b5ce18888e5cf335d1d688e67d4c4591af2c9
Alexander Metz <alexander.wt.metz@gmail.com>
2016-11-12 18:49:23 +0100

    font: add default parameters

---
+++ font/param.py
@@ -0,0 +1,8 @@
+# charsets
+PRINTABLE_ASCII = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;?@[\\]^_`{|}~ '
+# ...
+
+# fonts
+DEFAULT_FONT = 'DejaVuSansMono.ttf'
+DEFAULT_SIZE = 18
+DEFAULT_DIM = (11, 23)
