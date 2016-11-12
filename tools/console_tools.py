87302681f3a5070d14b97a0aebdd30f623889b16
Alexander Metz <alexander.wt.metz@gmail.com>
2016-11-12 18:50:38 +0100

    add console draw function

---
+++ tools/console_tools.py
@@ -0,0 +1,9 @@
+def draw(iterable2d):
+    for row in iterable2d:
+        out = ''
+        for col in row:
+            if col >= 0.5:
+                out += '#'
+            else:
+                out += ' '
+        print(out)
