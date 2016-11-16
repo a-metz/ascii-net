bf0fe66772ccebf8febfc6c6ac2ccf56a08e02d6
Alexander Metz <alexander.wt.metz@gmail.com>
2016-11-17 00:36:28 +0100

    font: add offset for rendering, clean up

--- font/render.py
+++ font/render.py
@@ -2,12 +2,12 @@ from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 
  # render a glyph to a PIL image
-def render_glyph_image(char, font, dim):
+def render_glyph_image(char, font, dim, offset=(0, 0)):
     img = Image.new('L', dim, color=0)
     # get draw handle
     dh = ImageDraw.Draw(img)
     # draw glyph
-    dh.text((0, 0), char, font=font, fill=255)
+    dh.text(offset, char, font=font, fill=255)
     return img
 
 
@@ -15,9 +15,5 @@ def convert_to_array(image):
     return np.asarray(image, dtype='float32') / 255
 
 
-# render all glyphs in charset
-def generate_glyphs(charset, fontname, fontsize, dim):
-    font = ImageFont.truetype(fontname, fontsize)
-    for char in charset:
-        glyph = convert_to_array(render_glyph_image(char, font, dim))
-        yield char, glyph
+def load_font(fontname, fontsize):
+    return ImageFont.truetype(fontname, fontsize)
