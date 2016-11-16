a8a673bb57e10dd73e26b07a52941a7eb5f49af9
Alexander Metz <alexander.wt.metz@gmail.com>
2016-11-17 00:39:09 +0100

    font: transform, refactor as function with context

--- font/transform.py
+++ font/transform.py
@@ -2,13 +2,16 @@ import scipy.ndimage as ndimage
 import numpy as np
 
 
-def random_affine(glyph, transpose_max, affine_max):
-    # random transpose: +-transpose_max
-    transpose = (np.random.random(2) - 0.5) * 2 * transpose_max
-    
-    # identity matrix (no transform)
-    affine = np.asarray([[1, 0], [0, 1]], dtype='float32')
-    # add randomness: +-affine_max
-    affine += (np.random.random((2,2)) - 0.5) * 2 * affine_max
-    
-    return ndimage.affine_transform(glyph, matrix=affine, offset=transpose, cval=0)
+def random_affine(transpose_scale, affine_scale, fill=0):
+    def transform(glyph):
+        # random transpose: +-transpose_max
+        transpose = np.random.normal(loc=0, scale=transpose_scale, size=2)
+        
+        # identity matrix (no transform)
+        affine = np.asarray([[1, 0], [0, 1]], dtype='float32')
+        # add randomness: +-affine_max
+        affine += np.random.normal(loc=0, scale=affine_scale, size=(2,2))
+
+        return ndimage.affine_transform(glyph, matrix=affine, offset=transpose, cval=fill)
+
+    return transform
