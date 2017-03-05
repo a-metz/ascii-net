cffde2528658e6160661a35cb63f8e07a7941e2c
Alexander Metz <alexander.wt.metz@gmail.com>
2017-03-05 16:32:40 +0100

    font/test_ocr: generate new training data for each epoch

--- model/keras/model.py
+++ model/keras/model.py
@@ -7,7 +7,7 @@ from keras.optimizers import SGD
 class OcrModel(object):
     def __init__(self, num_pixels, num_classes):
         self.model = Sequential()
-        self.model.add(Dense(output_dim=num_pixels, input_dim=num_pixels))
+        self.model.add(Dense(output_dim=num_pixels  , input_dim=num_pixels))
         self.model.add(Activation('sigmoid'))
         self.model.add(Dense(output_dim=num_classes))
         self.model.add(Activation('softmax'))
@@ -16,7 +16,7 @@ class OcrModel(object):
             loss='categorical_crossentropy',
             optimizer=SGD(lr=1, momentum=0.9, nesterov=True))
 
-    def train(self, inputs, labels, epochs):
+    def train(self, inputs, labels, epochs=1):
         history = self.model.fit(inputs,
                                  labels,
                                  batch_size=92,
--- test_ocr.py
+++ test_ocr.py
@@ -4,19 +4,19 @@ import numpy as np
 from font import training_data, param, transform, render
 from image import tiles, image_data
 
-# temporary batch loading of training data from new training data generator
-def default_training_data():
+
+# generate one batch of training data
+def generate_training_data(batch_size):
     fnt = render.load_font(fontname=param.DEFAULT_FONT, fontsize=param.DEFAULT_SIZE)
     trn = transform.random_affine(param.DEFAULT_TRANSPOSE_SCALE, param.DEFAULT_AFFINE_SCALE)
     gen, get_char, get_index = training_data.get_sample_generator(charset=param.PRINTABLE_ASCII, font=fnt, dim=param.DEFAULT_DIM,
             offset=param.DEFAULT_OFFSET, transform=trn)
 
-    batch_size = 92
     batch = list(training_data.batch(gen, batch_size))
-    input_len = len(batch[0][1].flatten())
-    inputs = np.zeros((batch_size, input_len))
-    label_len = len(batch[0][0])
-    labels = np.zeros((batch_size, label_len))
+    inputs_len = len(batch[0][1].flatten())
+    inputs = np.zeros((batch_size, inputs_len))
+    labels_len = len(batch[0][0])
+    labels = np.zeros((batch_size, labels_len))
 
     for index, tple in enumerate(batch):
         inputs[index, :] = tple[1].flatten()
@@ -27,21 +27,7 @@ def default_training_data():
     return inputs, labels, chars, get_char, get_index
 
 
-def train(model, inputs, labels, epochs, e_output=100):
-    for i in range(0, epochs, e_output):
-        train_loss = model.train(inputs, labels, e_output)
-
-        # predict class for all training data
-        p_classes = model.predict(inputs)
-
-        print('epoch: %i, loss: %f' % (i + e_output, train_loss))
-        #print('predict: %s' % ''.join(p_chars))
-        #print('errors: %s' % ''.join(p_errors))
-        #if len(p_errors) == 0:
-        #    break
-
-
-def generate(backend, epochs):
+def generate(backend, batch_size, epochs):
     if backend == 'keras':
         from model.keras import OcrModel
     elif backend == 'nnet':
@@ -51,36 +37,45 @@ def generate(backend, epochs):
 
     print('generate with %s model in %d epochs' % (backend, epochs))
 
-    print('load data')
-    inputs, labels, chars, get_char, get_index = default_training_data()
-
     t = list(tiles.read('test/test_image_w_inv.png', 9, 18))
     test_inputs = image_data.convert(t)
 
     print('load model')
+    inputs, labels, _, _, _ = generate_training_data(1)
     model = OcrModel(len(inputs[0]), len(labels[0]))
 
     print('start training')
-    # train for x epochs
-    train(model, inputs, labels, epochs)
+    
+    for i in range(0, epochs):
+        # generate batch of training data
+        inputs, labels, _, _, get_index = generate_training_data(batch_size)
+        
+        # train
+        train_loss = model.train(inputs, labels)
 
-    print('predict')
-    # predict class for test_inputs
-    p_classes = model.predict(test_inputs)
+        # predict class for all training data
+        p_classes = model.predict(inputs)
 
-    p_chars = [get_index(cl) for cl in p_classes]
-    for i in range(0, len(p_chars), 80):
-        print(''.join(p_chars[i:i + 80]))
+        print('epoch: %i, loss: %f' % (i, train_loss))
+
+        print('predict')
+        # predict class for test_inputs
+        p_classes = model.predict(test_inputs)
+
+        p_chars = [get_index(cl) for cl in p_classes]
+        for i in range(0, len(p_chars), 80):
+            print(''.join(p_chars[i:i + 80]))
 
 
 def test_generate_nnet():
-    generate('nnet', 100)
+    generate('nnet', 100, 1)
 
 
 def test_generate_keras():
-    generate('keras', 100)
+    generate('keras', 100, 1)
 
 
 if __name__ == "__main__":
     #generate('nnet', 1000)
-    generate('keras', 5000)
+    charset_size = len(param.PRINTABLE_ASCII)
+    generate('keras', batch_size=charset_size * 100, epochs=10000)
