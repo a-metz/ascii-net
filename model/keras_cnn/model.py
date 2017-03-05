44a41bc36440c7dbfc523b665bd9728c5e21565b
Alexander Metz <alexander.wt.metz@gmail.com>
2017-03-05 21:55:31 +0100

    keras_cnn: refactor/wrap mnist example as drop in replacement model

--- model/keras_cnn/model.py
+++ model/keras_cnn/model.py
@@ -1,27 +1,12 @@
-'''Trains a simple convnet on the MNIST dataset.
-
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
-from __future__ import print_function
 import numpy as np
-np.random.seed(1337)  # for reproducibility
 
-from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
-from keras import backend as K
 
 batch_size = 128
-nb_classes = 10
-nb_epoch = 12
 
-# input image dimensions
-img_rows, img_cols = 28, 28
 # number of convolutional filters to use
 nb_filters = 32
 # size of pooling area for max pooling
@@ -29,54 +14,42 @@ pool_size = (2, 2)
 # convolution kernel size
 kernel_size = (3, 3)
 
-# the data, shuffled and split between train and test sets
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
+class OcrModel(object):
+    def __init__(self, shape_pixels, num_classes):
+        self.input_shape = (1, ) + shape_pixels
 
-if K.image_dim_ordering() == 'th':
-    X_train = X_train.reshape(X_train.shape[0], 1, img_rows, img_cols)
-    X_test = X_test.reshape(X_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 1)
-    X_test = X_test.reshape(X_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+        self.model = Sequential()
+        self.model.add(Convolution2D(nb_filters,
+                                     kernel_size[0],
+                                     kernel_size[1],
+                                     border_mode='valid',
+                                     input_shape=self.input_shape))
+        self.model.add(Activation('relu'))
+        self.model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
+        self.model.add(Activation('relu'))
+        self.model.add(MaxPooling2D(pool_size=pool_size))
+        self.model.add(Dropout(0.25))
 
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255
-X_test /= 255
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+        self.model.add(Flatten())
+        self.model.add(Dense(128))
+        self.model.add(Activation('relu'))
+        self.model.add(Dropout(0.5))
+        self.model.add(Dense(num_classes))
+        self.model.add(Activation('softmax'))
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+        self.model.compile(loss='categorical_crossentropy',
+                           optimizer='adadelta')
 
-model = Sequential()
+    def reshape_inputs(self, inputs):
+        return inputs.reshape((-1, ) + self.input_shape)
 
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                        border_mode='valid',
-                        input_shape=input_shape))
-model.add(Activation('relu'))
-model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(0.25))
+    def train(self, inputs, labels, epochs=1):
+        history = self.model.fit(self.reshape_inputs(inputs),
+                                 labels,
+                                 batch_size=batch_size,
+                                 nb_epoch=epochs,
+                                 verbose=1)
+        return history.history['loss'][-1]
 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
-
-model.compile(loss='categorical_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
-
-model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-          verbose=1, validation_data=(X_test, Y_test))
-score = model.evaluate(X_test, Y_test, verbose=0)
-print('Test score:', score[0])
-print('Test accuracy:', score[1])
+    def predict(self, inputs):
+        return self.model.predict_classes(self.reshape_inputs(inputs), verbose=0)
